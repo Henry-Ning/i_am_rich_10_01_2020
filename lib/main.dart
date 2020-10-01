503ea21439d6732dc4b896a7f6d9c0dbfb4b9bba
Henry-Ning <henryhuning@gmail.com>
2020-10-01 15:43:27 -0700

    28. Working with Assets in Flutter & the Pubspec file 9min

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage('https://i.pinimg.com/originals/bc/31/f8/bc31f8c1024fd8ff04542e801ec391bd.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
         appBar: AppBar(
